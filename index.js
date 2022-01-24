da43e9825379cdf916bb1b2ea6c51d6aea28af98
MrMikol <michaelneiltan04@gmail.com>
2022-01-24 03:32:23 +0000

    added map

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ function decrease() {
     counting.innerText = minus
 }
 
+function initMap() {
+    var location = {lat: 10.641420, lng: -61.400574};
+    var map = new google.maps.Map(document.getElementById("sample-map"), {zoom: 13, center: location});
+    var marker = new google.maps.Marker({
+        position: location, 
+        map: map
+    });
+}
